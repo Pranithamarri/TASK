69d4563b2eb83500a6313875ec58ebf85df084d5
Pranithamarri <pranithamarri28@gmail.com>
2026-04-29 23:09:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return """
+    <h1>🛒 Grocery Mart</h1>
+    <ul>
+        <li>🍎 Apples - ₹100/kg</li>
+        <li>🥔 Potatoes - ₹40/kg</li>
+        <li>🥛 Milk - ₹50/L</li>
+        <li>🍞 Bread - ₹30</li>
+    </ul>
+    """
+
+app.run(host='0.0.0.0', port=5000)
